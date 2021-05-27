f7496bf38317e7c3cd55b636cc09ae59fabe34b1
Peter Hrvola <peter.hrvola@wundermanthompson.com>
2021-05-27 23:41:08 +0200

    remove azure

--- python.py
+++ python.py
@@ -3,9 +3,11 @@ import platform
 import os
 import sys
 
+cwd = '/'.join((__file__).split('/')[:-1])
+
 if platform.system() == 'Darwin':
   os.environ['PYTHONHOME'] = 'macos'
-  os.execv('macos/bin/python3.7', sys.argv)
+  os.execv(cwd + '/macos/bin/python3.7', sys.argv)
 else:
   os.environ['PYTHONHOME'] = 'linux'
-  os.execv('linux/bin/python3.7', sys.argv)
+  os.execv(cwd + '/linux/bin/python3.7', sys.argv)
