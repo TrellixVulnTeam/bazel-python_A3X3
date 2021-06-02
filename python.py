ceece1ffe8481c2f2dc4a89b0f6f530dbd6ca6f2
Peter Hrvola <peter.hrvola@wundermanthompson.com>
2021-06-02 23:43:17 +0200

    a

--- python.py
+++ python.py
@@ -4,8 +4,11 @@ import os
 import sys
 
 cwd = '/'.join((__file__).split('/')[:-1])
-
+print(os.environ)
+print(cwd)
 if platform.system() == 'Darwin':
+  os.environ['PYTHONHOME'] = cwd + '/macos'
   os.execv(cwd + '/macos/bin/python3.7', sys.argv)
 else:
+  os.environ['PYTHONHOME'] = cwd + '/linux'
   os.execv(cwd + '/linux/bin/python3.7', sys.argv)
