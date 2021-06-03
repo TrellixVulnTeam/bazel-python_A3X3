d49c1947244fc3e7276de106d4405659a206c34f
Peter Hrvola <peter.hrvola@wundermanthompson.com>
2021-06-03 14:10:47 +0200

    env

--- python.py
+++ python.py
@@ -5,8 +5,9 @@ import sys
 import subprocess
 
 cwd = '/'.join((__file__).split('/')[:-1])
-
+print(sys.argv[1:])
+print(os.environ)
 if platform.system() == 'Darwin':
-  subprocess.run([cwd + '/macos/bin/python3.7'] + sys.argv[1:], env= {'PYTHONHOME': cwd + '/macos' }, check=True)
+  subprocess.run([cwd + '/macos/bin/python3.7'] + sys.argv[1:], env= {'PYTHONHOME': cwd + '/macos', 'PYTHONPATH': os.environ.get('PYTHONPATH') }, check=True)
 else:
-  subprocess.run([cwd + '/linux/bin/python3.7'] + sys.argv[1:], env= {'PYTHONHOME': cwd + '/linux' }, check=True)
+  subprocess.run([cwd + '/linux/bin/python3.7'] + sys.argv[1:], env= {'PYTHONHOME': cwd + '/linux', 'PYTHONPATH': os.environ.get('PYTHONPATH') }, check=True)
